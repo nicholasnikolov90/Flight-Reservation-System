cd4f717c3685a8d929e8f2a9dc53daca922bc401
Nick <35607793+nicholasnikolov90@users.noreply.github.com>
2023-11-22 11:22:53 -0700

    finished flight CRUD functionality

--- server/app/urls.py
+++ server/app/urls.py
@@ -4,6 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.flight_overview, name='flight_overview'),
-    path('flight-list/', views.show_all_flights, name='flight-list')
+    path('flight-list/', views.show_all_flights, name='flight-list'), #URL for a list of all flights
+    path('flight-detail/<int:pk>/', views.show_flight, name='flight-detail'), #URL for a single flight
+    path('flight-create/', views.create_flight, name='flight-create'), #adds a new flight to the database
+    path('flight-update/<int:pk>', views.update_flight, name='flight-update'), #modifies an existing flight in the database
+        path('flight-delete/<int:pk>', views.delete_flight, name='flight-delete'), #deletes an existing flight in the database
+
+
 
 ]
--- server/app/views.py
+++ server/app/views.py
@@ -7,7 +7,22 @@ from rest_framework.decorators import api_view
 from .serializers import FlightSerializer
 from .models import Flight
 
+"""
+FLIGHT VIEWS
 
+FUNCTIONS:
+flight_overview - shows which URL's are dedicated to flights
+show_all_flights - shows a list of all flights in the database (returns ALL info for ALL flights)
+show_flight - shows info for a single flight
+create_flight - add a new flight to the database
+update_flight - modify an existing flight
+delete_flight - delete an existing flight
+
+
+
+"""
+
+#shows all of the URLS related to flights
 @api_view(['GET'])
 #common views:
 def flight_overview(request):
@@ -20,18 +35,49 @@ def flight_overview(request):
     }
     return Response(flight_urls)
 
+#returns a list of all flights in the database
 @api_view(['GET'])
 def show_all_flights(request):
     flights = Flight.objects.all()
     serializer = FlightSerializer(flights, many=True)
     return Response(serializer.data)
 
+#return the information for a single flight, by entering its privatekey (pk)
+@api_view(['GET'])
+def show_flight(request, pk):
+    flight = Flight.objects.get(id=pk)
+    serializer = FlightSerializer(flight, many=False)
+    return Response(serializer.data)
+
+#create a new flight
+@api_view(['POST'])
+def create_flight(request):
+    serializer = FlightSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+#Update an existing flight
+@api_view(['POST'])
+def update_flight(request, pk):
+    flight = Flight.objects.get(id=pk)
+    serializer = FlightSerializer(instance = flight, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+#delete an existing flight
+@api_view(['GET'])
+def delete_flight(request, pk):
+    flight = Flight.objects.get(id=pk)
+    flight.delete()
+
+    return Response('Flight deleted successfully')
+
+
 
 #USER VIEWS
-#def user_form(request):
-
-
-
 
 # Create your views here.
 def book_ticket(request, user_id, flight_id):
