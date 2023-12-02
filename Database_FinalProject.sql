2e332f266bf79d834dd95b10912da25da60a26db
J-Celis <132500159+J-Celis@users.noreply.github.com>
2023-12-02 13:23:02 -0700

    Added all information for flight 1

--- Database_FinalProject.sql
+++ Database_FinalProject.sql
@@ -84,85 +84,14 @@ INSERT INTO Plane (plane_model, seating_capacity) VALUES
 
 INSERT INTO Flight (origin, destination, date, departure_time, arrival_time, plane_ID)
 VALUES
-('JFK', 'LAX', '2023-11-22', '12:00:00', '15:30:00', 1),
-('LHR', 'CDG', '2023-11-23', '09:30:00', '11:00:00', 2),
-('SFO', 'SEA', '2023-11-24', '14:45:00', '16:15:00', 3),
-('HND', 'ICN', '2023-11-25', '18:00:00', '21:30:00', 4),
-('SYD', 'LAX', '2023-11-26', '08:00:00', '14:00:00', 5),
-('DXB', 'LHR', '2023-11-27', '16:30:00', '23:00:00', 6),
-('ATL', 'ORD', '2023-11-28', '10:15:00', '12:30:00', 7),
-('PEK', 'HKG', '2023-11-29', '13:45:00', '15:30:00', 8),
-('AMS', 'CDG', '2023-11-30', '11:00:00', '12:15:00', 9),
-('LAX', 'JFK', '2023-12-01', '19:30:00', '23:00:00', 10);
+('JFK', 'LAX', '2023-11-22', '12:00:00', '15:30:00', 1);
 
+-- Comfort seats
 INSERT INTO Seat (flight_ID, seat_number, seat_type, availability, price)
 VALUES
-(1, 1, 'B', 1, 500.00),
-(1, 2, 'O', 1, 250.00),
+(1, 1, 'C', 1, 350.00),
+(1, 2, 'C', 1, 350.00),
 (1, 3, 'C', 1, 350.00),
-(2, 1, 'B', 1, 450.00),
-(2, 2, 'O', 1, 300.00),
-(2, 3, 'C', 1, 400.00),
-(3, 1, 'B', 1, 550.00),
-(3, 2, 'O', 1, 350.00),
-(3, 3, 'C', 1, 450.00),
-(4, 1, 'B', 1, 600.00);
-
-INSERT INTO User (seat_ID, flight_ID)
-VALUES
-(1, 1),
-(2, 1),
-(3, 2),
-(4, 2),
-(5, 3),
-(6, 3),
-(7, 4),
-(8, 4),
-(9, 5),
-(10, 5);
-
-INSERT INTO registeredUser (user_ID, first_name, last_name, address)
-VALUES
-(1, 'John', 'Doe', '123 Main St'),
-(2, 'Jane', 'Smith', '456 Oak St'),
-(3, 'Bob', 'Johnson', '789 Pine St'),
-(4, 'Alice', 'Williams', '456 Elm St'),
-(5, 'Charlie', 'Brown', '789 Maple St'),
-(6, 'Eva', 'Davis', '123 Birch St'),
-(7, 'Frank', 'Martin', '456 Cedar St'),
-(8, 'Grace', 'Taylor', '789 Oak St'),
-(9, 'Henry', 'Clark', '123 Pine St'),
-(10, 'Isabel', 'Anderson', '456 Maple St');
-
-INSERT INTO Crew (first_name, last_name, flight_ID)
-VALUES
-('Alice', 'Pilot', 1),
-('Bob', 'Attendant', 1),
-('Charlie', 'Pilot', 2),
-('David', 'Attendant', 2),
-('Eva', 'Pilot', 3),
-('Frank', 'Attendant', 3),
-('Grace', 'Pilot', 4),
-('Henry', 'Attendant', 4),
-('Isabel', 'Pilot', 5),
-('Jack', 'Attendant', 5);
-
-INSERT INTO Booking (user_ID, flight_ID, insurance)
-VALUES
-(1, 1, 1),
-(2, 1, 0),
-(3, 2, 1),
-(4, 2, 0),
-(5, 3, 1),
-(6, 3, 1),
-(7, 4, 0),
-(8, 4, 1),
-(9, 5, 0),
-(10, 5, 1);
-
--- all comfort seats
-INSERT INTO Seat (flight_ID, seat_number, seat_type, availability, price)
-VALUES
 (1, 4, 'C', 1, 350.00),
 (1, 5, 'C', 1, 350.00),
 (1, 6, 'C', 1, 350.00),
@@ -171,14 +100,14 @@ VALUES
 (1, 9, 'C', 1, 350.00),
 (1, 10, 'C', 1, 350.00),
 (1, 11, 'C', 1, 350.00),
-(1, 12, 'C', 1, 350.00),
-(1, 13, 'C', 1, 350.00),
-(1, 14, 'C', 1, 350.00),
-(1, 15, 'C', 1, 350.00);
+(1, 12, 'C', 1, 350.00);
 
--- All business seats
+-- Business seats
 INSERT INTO Seat (flight_ID, seat_number, seat_type, availability, price)
 VALUES
+(1, 13, 'B', 1, 500.00),
+(1, 14, 'B', 1, 500.00),
+(1, 15, 'B', 1, 500.00),
 (1, 16, 'B', 1, 500.00),
 (1, 17, 'B', 1, 500.00),
 (1, 18, 'B', 1, 500.00),
@@ -187,14 +116,14 @@ VALUES
 (1, 21, 'B', 1, 500.00),
 (1, 22, 'B', 1, 500.00),
 (1, 23, 'B', 1, 500.00),
-(1, 24, 'B', 1, 500.00),
-(1, 25, 'B', 1, 500.00),
-(1, 26, 'B', 1, 500.00),
-(1, 27, 'B', 1, 500.00);
+(1, 24, 'B', 1, 500.00);
 
--- All ordinary seats
+-- Ordinary seats
 INSERT INTO Seat (flight_ID, seat_number, seat_type, availability, price)
 VALUES
+(1, 25, 'O', 1, 250.00),
+(1, 26, 'O', 1, 250.00),
+(1, 27, 'O', 1, 250.00),
 (1, 28, 'O', 1, 250.00),
 (1, 29, 'O', 1, 250.00),
 (1, 30, 'O', 1, 250.00),
@@ -217,26 +146,12 @@ VALUES
 (1, 47, 'O', 1, 250.00),
 (1, 48, 'O', 1, 250.00);
 
-update Seat
-set seat_type = 'C', price = 350
-where flight_ID = 1 and seat_number between 1 and 12;
-
-update Seat
-set seat_type = 'B', price = 500
-where flight_ID = 1 and seat_number between 13 and 24;
-
-update Seat 
-set seat_type = 'O', price = 250
-where flight_ID = 1 and seat_number between 25 and 48;
-
--- select * 
--- from Seat
--- where flight_ID = 1
--- order by seat_number asc;
-
--- Comfort seats
+-- Comfort seats users
 INSERT INTO User (seat_ID, flight_ID)
 VALUES
+(1, 1),
+(2, 1),
+(3, 1),
 (4, 1),
 (5, 1),
 (6, 1),
@@ -245,14 +160,14 @@ VALUES
 (9, 1),
 (10, 1),
 (11, 1),
-(12, 1),
-(13, 1),
-(14, 1),
-(15, 1);
+(12, 1);
 
--- Business seats
+-- Business seats users
 INSERT INTO User (seat_ID, flight_ID)
 VALUES
+(13, 1),
+(14, 1),
+(15, 1),
 (16, 1),
 (17, 1),
 (18, 1),
@@ -261,14 +176,14 @@ VALUES
 (21, 1),
 (22, 1),
 (23, 1),
-(24, 1),
-(25, 1),
-(26, 1),
-(27, 1);
+(24, 1);
 
--- Ordinary seats
+-- Ordinary seats users
 INSERT INTO User (seat_ID, flight_ID)
 VALUES
+(25, 1),
+(26, 1),
+(27, 1),
 (28, 1),
 (29, 1),
 (30, 1),
@@ -291,125 +206,125 @@ VALUES
 (47, 1),
 (48, 1);
 
--- select *
--- from User
--- where flight_ID = 1;
-
-INSERT INTO registeredUser (user_ID, first_name, last_name, address)
+-- Comfort seats registered users
+INSERT INTO registeredUser (user_ID, first_name, last_name, address, user_name, pass_word)
 VALUES
-(11, 'Emma', 'Johnson', '123 Main St, Apt 11'),
-(12, 'Noah', 'Smith', '456 Oak St, Apt 12'),
-(13, 'Olivia', 'Williams', '789 Pine St, Apt 13'),
-(14, 'Liam', 'Brown', '456 Elm St, Apt 14'),
-(15, 'Ava', 'Jones', '789 Maple St, Apt 15'),
-(16, 'Mia', 'Davis', '123 Birch St, Apt 16'),
-(17, 'Sophia', 'Taylor', '456 Cedar St, Apt 17'),
-(18, 'Jackson', 'Clark', '789 Oak St, Apt 18'),
-(19, 'Lucas', 'Anderson', '123 Pine St, Apt 19'),
-(20, 'Lily', 'Martinez', '456 Maple St, Apt 20'),
-(21, 'Logan', 'Hernandez', '789 Elm St, Apt 21'),
-(22, 'Emma', 'Garcia', '123 Cedar St, Apt 22'),
-(23, 'Aiden', 'Rodriguez', '456 Birch St, Apt 23'),
-(24, 'Ella', 'Lopez', '789 Cedar St, Apt 24'),
-(25, 'Caden', 'Perez', '123 Maple St, Apt 25'),
-(26, 'Aria', 'Williams', '456 Oak St, Apt 26'),
-(27, 'Grayson', 'Smith', '789 Pine St, Apt 27'),
-(28, 'Amelia', 'Davis', '123 Elm St, Apt 28'),
-(29, 'Mason', 'Taylor', '456 Cedar St, Apt 29'),
-(30, 'Harper', 'Clark', '789 Oak St, Apt 30'),
-(31, 'Ethan', 'Anderson', '123 Pine St, Apt 31'),
-(32, 'Charlotte', 'Martinez', '456 Maple St, Apt 32'),
-(33, 'Oliver', 'Hernandez', '789 Elm St, Apt 33'),
-(34, 'Isabella', 'Garcia', '123 Cedar St, Apt 34'),
-(35, 'Sophie', 'Rodriguez', '456 Birch St, Apt 35'),
-(36, 'Benjamin', 'Lopez', '789 Cedar St, Apt 36'),
-(37, 'Aiden', 'Perez', '123 Maple St, Apt 37'),
-(38, 'Evelyn', 'Williams', '456 Oak St, Apt 38'),
-(39, 'Leo', 'Smith', '789 Pine St, Apt 39'),
-(40, 'Avery', 'Davis', '123 Elm St, Apt 40'),
-(41, 'Jack', 'Taylor', '456 Cedar St, Apt 41'),
-(42, 'Madison', 'Clark', '789 Oak St, Apt 42'),
-(43, 'Scarlett', 'Anderson', '123 Pine St, Apt 43'),
-(44, 'Gabriel', 'Martinez', '456 Maple St, Apt 44'),
-(45, 'Riley', 'Hernandez', '789 Elm St, Apt 45'),
-(46, 'David', 'Garcia', '123 Cedar St, Apt 46'),
-(47, 'Zoe', 'Rodriguez', '456 Birch St, Apt 47'),
-(48, 'Julian', 'Lopez', '789 Cedar St, Apt 48'),
-(49, 'Leah', 'Perez', '123 Maple St, Apt 49'),
-(50, 'Jackson', 'Williams', '456 Oak St, Apt 50'),
-(51, 'Chloe', 'Smith', '789 Pine St, Apt 51'),
-(52, 'Lucy', 'Davis', '123 Elm St, Apt 52'),
-(53, 'Ryan', 'Taylor', '456 Cedar St, Apt 53'),
-(54, 'Ellie', 'Clark', '789 Oak St, Apt 54'),
-(55, 'Nathan', 'Anderson', '123 Pine St, Apt 55');
+(1, 'John', 'Doe', '123 Comfort St', 'john_doe', 'password1'),
+(2, 'Jane', 'Smith', '456 Comfort St', 'jane_smith', 'password2'),
+(3, 'User', '3', '789 Comfort St', 'user3', 'password3'),
+(4, 'User', '4', '101 Comfort St', 'user4', 'password4'),
+(5, 'User', '5', '202 Comfort St', 'user5', 'password5'),
+(6, 'User', '6', '303 Comfort St', 'user6', 'password6'),
+(7, 'User', '7', '404 Comfort St', 'user7', 'password7'),
+(8, 'User', '8', '505 Comfort St', 'user8', 'password8'),
+(9, 'User', '9', '606 Comfort St', 'user9', 'password9'),
+(10, 'User', '10', '707 Comfort St', 'user10', 'password10'),
+(11, 'User', '11', '808 Comfort St', 'user11', 'password11'),
+(12, 'User', '12', '909 Comfort St', 'user12', 'password12');
 
--- select *
--- from registeredUser
+-- Business seats registered users
+INSERT INTO registeredUser (user_ID, first_name, last_name, address, user_name, pass_word)
+VALUES
+(13, 'Business', 'User1', '123 Business St', 'business_user1', 'password13'),
+(14, 'Business', 'User2', '456 Business St', 'business_user2', 'password14'),
+(15, 'Business', 'User3', '789 Business St', 'business_user3', 'password15'),
+(16, 'Business', 'User4', '101 Business St', 'business_user4', 'password16'),
+(17, 'Business', 'User5', '202 Business St', 'business_user5', 'password17'),
+(18, 'Business', 'User6', '303 Business St', 'business_user6', 'password18'),
+(19, 'Business', 'User7', '404 Business St', 'business_user7', 'password19'),
+(20, 'Business', 'User8', '505 Business St', 'business_user8', 'password20'),
+(21, 'Business', 'User9', '606 Business St', 'business_user9', 'password21'),
+(22, 'Business', 'User10', '707 Business St', 'business_user10', 'password22'),
+(23, 'Business', 'User11', '808 Business St', 'business_user11', 'password23'),
+(24, 'Business', 'User12', '909 Business St', 'business_user12', 'password24');
 
+-- Ordinary seats registered users
+INSERT INTO registeredUser (user_ID, first_name, last_name, address, user_name, pass_word)
+VALUES
+(25, 'Ordinary', 'User1', '123 Ordinary St', 'ordinary_user1', 'password25'),
+(26, 'Ordinary', 'User2', '456 Ordinary St', 'ordinary_user2', 'password26'),
+(27, 'Ordinary', 'User3', '789 Ordinary St', 'ordinary_user3', 'password27'),
+(28, 'Ordinary', 'User4', '101 Ordinary St', 'ordinary_user4', 'password28'),
+(29, 'Ordinary', 'User5', '202 Ordinary St', 'ordinary_user5', 'password29'),
+(30, 'Ordinary', 'User6', '303 Ordinary St', 'ordinary_user6', 'password30'),
+(31, 'Ordinary', 'User7', '404 Ordinary St', 'ordinary_user7', 'password31'),
+(32, 'Ordinary', 'User8', '505 Ordinary St', 'ordinary_user8', 'password32'),
+(33, 'Ordinary', 'User9', '606 Ordinary St', 'ordinary_user9', 'password33'),
+(34, 'Ordinary', 'User10', '707 Ordinary St', 'ordinary_user10', 'password34'),
+(35, 'Ordinary', 'User11', '808 Ordinary St', 'ordinary_user11', 'password35'),
+(36, 'Ordinary', 'User12', '909 Ordinary St', 'ordinary_user12', 'password36'),
+(37, 'Ordinary', 'User13', '111 Ordinary St', 'ordinary_user13', 'password37'),
+(38, 'Ordinary', 'User14', '222 Ordinary St', 'ordinary_user14', 'password38'),
+(39, 'Ordinary', 'User15', '333 Ordinary St', 'ordinary_user15', 'password39'),
+(40, 'Ordinary', 'User16', '444 Ordinary St', 'ordinary_user16', 'password40'),
+(41, 'Ordinary', 'User17', '555 Ordinary St', 'ordinary_user17', 'password41'),
+(42, 'Ordinary', 'User18', '666 Ordinary St', 'ordinary_user18', 'password42'),
+(43, 'Ordinary', 'User19', '777 Ordinary St', 'ordinary_user19', 'password43'),
+(44, 'Ordinary', 'User20', '888 Ordinary St', 'ordinary_user20', 'password44'),
+(45, 'Ordinary', 'User21', '999 Ordinary St', 'ordinary_user21', 'password45'),
+(46, 'Ordinary', 'User22', '000 Ordinary St', 'ordinary_user22', 'password46'),
+(47, 'Ordinary', 'User23', '1234 Ordinary St', 'ordinary_user23', 'password47'),
+(48, 'Ordinary', 'User24', '5678 Ordinary St', 'ordinary_user24', 'password48');
+
+-- Crew for flight_ID = 1
+INSERT INTO Crew (first_name, last_name, flight_ID)
+VALUES
+('Captain', 'Smith', 1),
+('Co-Pilot', 'Johnson', 1),
+('Flight Attendant', 'Anderson', 1),
+('Flight Attendant', 'Williams', 1),
+('Flight Attendant', 'Jones', 1);
+
+-- Booking entries for flight_ID = 1
 INSERT INTO Booking (user_ID, flight_ID, insurance)
 VALUES
+(1, 1, 1),
+(2, 1, 0),
+(3, 1, 1),
+(4, 1, 0),
+(5, 1, 1),
+(6, 1, 0),
+(7, 1, 1),
+(8, 1, 0),
+(9, 1, 1),
+(10, 1, 0),
 (11, 1, 1),
 (12, 1, 0),
-(13, 2, 1),
-(14, 2, 0),
-(15, 3, 1),
-(16, 3, 1),
-(17, 4, 0),
-(18, 4, 1),
-(19, 5, 0),
-(20, 5, 1),
+(13, 1, 1),
+(14, 1, 0),
+(15, 1, 1),
+(16, 1, 0),
+(17, 1, 1),
+(18, 1, 0),
+(19, 1, 1),
+(20, 1, 0),
 (21, 1, 1),
 (22, 1, 0),
-(23, 2, 1),
-(24, 2, 0),
-(25, 3, 1),
-(26, 3, 1),
-(27, 4, 0),
-(28, 4, 1),
-(29, 5, 0),
-(30, 5, 1),
+(23, 1, 1),
+(24, 1, 0),
+(25, 1, 1),
+(26, 1, 0),
+(27, 1, 1),
+(28, 1, 0),
+(29, 1, 1),
+(30, 1, 0),
 (31, 1, 1),
 (32, 1, 0),
-(33, 2, 1),
-(34, 2, 0),
-(35, 3, 1),
-(36, 3, 1),
-(37, 4, 0),
-(38, 4, 1),
-(39, 5, 0),
-(40, 5, 1),
+(33, 1, 1),
+(34, 1, 0),
+(35, 1, 1),
+(36, 1, 0),
+(37, 1, 1),
+(38, 1, 0),
+(39, 1, 1),
+(40, 1, 0),
 (41, 1, 1),
 (42, 1, 0),
-(43, 2, 1),
-(44, 2, 0),
-(45, 3, 1),
-(46, 3, 1),
-(47, 4, 0),
-(48, 4, 1),
-(49, 5, 0),
-(50, 5, 1),
-(51, 1, 1),
-(52, 1, 0),
-(53, 2, 1),
-(54, 2, 0),
-(55, 3, 1);
+(43, 1, 1),
+(44, 1, 0),
+(45, 1, 1),
+(46, 1, 0),
+(47, 1, 1),
+(48, 1, 0);
 
-DELETE FROM Booking WHERE flight_ID <> 1; -- Delete Booking records not related to flight_ID = 1 GOOD 
 
-DELETE FROM registeredUser WHERE user_ID NOT IN (SELECT user_ID FROM Booking WHERE flight_ID = 1) LIMIT 1000; -- Delete registeredUser records not related to flight_ID = 1 GOOD
-
-DELETE FROM User WHERE flight_ID <> 1; -- Delete User records not related to flight_ID = 1 GOOD
-
-DELETE FROM Crew WHERE flight_ID <> 1; -- GOOD
-
--- Finally, delete records from the parent table
-DELETE FROM Seat WHERE flight_ID <> 1;
-
-DELETE FROM Flight WHERE flight_ID <> 1;
-
-SET SQL_SAFE_UPDATES = 0;
-DELETE FROM Plane WHERE plane_ID NOT IN (SELECT plane_ID FROM Flight WHERE flight_ID = 1) AND plane_ID IS NOT NULL;
-SET SQL_SAFE_UPDATES = 1;
-
-select *
-from Seat
